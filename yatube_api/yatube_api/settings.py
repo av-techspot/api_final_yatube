1a0e5a8600d66dfc3acb42d2e210dd8fc1bfbd63
Andrey Vasilev <philobeat@gmail.com>
2023-03-17 14:55:51 +0300

    Fix flake8 error

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     # Устанавливаем срок жизни токена
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
